b69c287f5e37649bb7ea1affa8314888d6b733f2
angelside <aff@canversoft.net>
2017-11-01 20:21:49 +0000

    Dosyalar eklendi

---
+++ tr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password'  => 'Parolalar en az altı karakter olmalı ve onay ile eşleşmelidir.',
+    'reset'     => 'Parolanız başarıyla sıfırlandı!',
+    'sent'      => 'Parola sıfırlama bileti e-posta ile gönderildi!',
+    'token'     => 'Parola sıfırlama bileti geçersiz.',
+    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+
+];
